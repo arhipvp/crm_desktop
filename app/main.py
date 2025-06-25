680cf6b15dc3ab01c13455b306575b9e519b0d22
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-06-25 17:13:59 +0300

    Add basic FastAPI web interface

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from .api.router import router as api_router
+from .web.router import router as web_router
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
+app.include_router(web_router)
 
 @app.get("/ping")
 def ping():
