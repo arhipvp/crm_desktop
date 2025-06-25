e0c35d12dc4368aee508c171a1cb88d30a6ec642
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-06-25 17:52:24 +0300

    Create database tables on startup

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 
 from .api.router import router as api_router
 from .web.router import router as web_router
+from .db import engine
+from .models import Base
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 app.include_router(api_router, prefix="/api")
 app.include_router(web_router)
 
