6b6ac9215157c1a374ce26162155d39ae40c0933
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-07-27 16:25:10 +0200

    feat: strip letters when normalizing numbers

--- tests/test_normalize_number.py
+++ tests/test_normalize_number.py
@@ -8,3 +8,6 @@ def test_normalize_number():
     assert normalize_number("1\u00a0234,5") == "1234.5"
     assert normalize_number(None) is None
     assert normalize_number(1234) == "1234"
+    assert normalize_number("6234.70p") == "6234.70"
+    assert normalize_number("123руб.") == "123"
+    assert normalize_number("456,78 р.") == "456.78"
