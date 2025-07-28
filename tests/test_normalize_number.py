f495956be03497d57fa22130a51592255f2be811
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-07-28 21:19:18 +0200

    test: add case for space with currency

--- tests/test_normalize_number.py
+++ tests/test_normalize_number.py
@@ -12,3 +12,4 @@ def test_normalize_number():
     assert normalize_number("123руб.") == "123"
     assert normalize_number("456,78 р.") == "456.78"
     assert normalize_number("4101.44RUB") == "4101.44"
+    assert normalize_number("4101.44 RUB") == "4101.44"
