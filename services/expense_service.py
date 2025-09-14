0305937888df2adf080c0953a572933beb93ac0d
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-09-14 22:30:13 +0200

    Расширен group_by в build_expense_query

--- services/expense_service.py
+++ services/expense_service.py
@@ -325,7 +325,7 @@ def build_expense_query(
             JOIN.LEFT_OUTER,
             on=(expense_subquery.c.payment_id == Payment.id),
         )
-        .group_by(Expense.id)
+        .group_by(Expense.id, Payment.id, Policy.id, Client.id, Deal.id)
     )
     query = apply_expense_filters(
         query,
