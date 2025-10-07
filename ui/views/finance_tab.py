6a952d852af5ede9b143f4bd72754b32a0a6622b
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-10-07 13:07:33 +0200

    Отложенная загрузка вкладок финансов

--- ui/views/finance_tab.py
+++ ui/views/finance_tab.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from PySide6.QtWidgets import QTabWidget, QVBoxLayout, QWidget
 
 from core.app_context import AppContext
@@ -29,11 +31,48 @@ class FinanceTab(QWidget):
         self.payment_view = payment_view_factory(
             parent=self, context=self._context
         )
-        self.income_view = income_view_factory(parent=self, context=self._context)
-        self.expense_view = expense_view_factory(parent=self, context=self._context)
+        self.income_view = income_view_factory(
+            parent=self, context=self._context, autoload=False
+        )
+        self.expense_view = expense_view_factory(
+            parent=self, context=self._context, autoload=False
+        )
 
         self.tabs.addTab(self.payment_view, "Платежи")
         self.tabs.addTab(self.income_view, "Доходы")
         self.tabs.addTab(self.expense_view, "Расходы")
 
+        self._loaded_tabs: dict[QWidget, bool] = {}
+        self._data_loaded_handlers: list[Callable[[int], None]] = []
+        for view in (self.payment_view, self.income_view, self.expense_view):
+            self._loaded_tabs[view] = bool(getattr(view, "model", None))
+            data_loaded = getattr(view, "data_loaded", None)
+            connect = getattr(data_loaded, "connect", None)
+            if callable(connect):
+                handler = self._create_data_loaded_handler(view)
+                connect(handler)
+                self._data_loaded_handlers.append(handler)
+
+        self.tabs.currentChanged.connect(self._on_tab_changed)
         self.tabs.setCurrentIndex(0)
+        self._on_tab_changed(self.tabs.currentIndex())
+
+    def _create_data_loaded_handler(self, view: QWidget) -> Callable[[int], None]:
+        def _handler(_count: int) -> None:
+            self._loaded_tabs[view] = True
+
+        return _handler
+
+    def _on_tab_changed(self, index: int) -> None:
+        widget = self.tabs.widget(index)
+        if widget is None:
+            return
+        if widget not in self._loaded_tabs:
+            return
+        if self._loaded_tabs[widget]:
+            return
+        load_data = getattr(widget, "load_data", None)
+        if callable(load_data):
+            load_data()
+        else:
+            self._loaded_tabs[widget] = True
